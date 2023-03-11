986c8a867455368cbf6dc9272b50bd57f907cb50
IrfanSuparman <93828619+IrfanSuparman@users.noreply.github.com>
2023-03-11 18:37:09 +0700

    menambah koneksi toko

---
+++ koneksi_toko.php
@@ -0,0 +1,6 @@
+<?php
+$host = "$ip";
+$user = "kasir";
+$pass = "ydUcgx+VcZOXOvtX8CgOQerivop3oMXGk=WosaavE+Cm";
+$nama_db = "pos"; //nama database
+$koneksi1 = mysqli_connect($host, $user, $pass, $nama_db); //pastikan urutan nya seperti ini, jangan tertukar
