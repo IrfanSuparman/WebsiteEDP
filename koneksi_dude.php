2c66ab13a3d9e9c207674aa530dd7744d9114846
IrfanSuparman <93828619+IrfanSuparman@users.noreply.github.com>
2023-03-11 18:37:51 +0700

    menambahkan koneksi berdasarakan IP Toko

---
+++ koneksi_dude.php
@@ -0,0 +1,6 @@
+<?php
+  $host = "192.168.85.208"; 
+  $user = "root";
+  $pass = "rahasiajkt2";
+  $nama_db = "indomaret";
+  $koneksi = mysqli_connect($host,$user,$pass,$nama_db); //pastikan urutan nya seperti ini, jangan tertukar
